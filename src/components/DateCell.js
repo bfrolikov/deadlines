7d4949ee5f941e458249a4ca6aaa7b5f847795b5
b.frolikov <b.frolikov@yandex.ru>
2021-03-13 16:11:43 +0300

    removed the bad code from Table.js

--- src/components/DateCell.js
+++ src/components/DateCell.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import moment from 'moment';
 import styled from 'styled-components';
-import 'moment/locale/ru';
 import { Header } from 'semantic-ui-react';
-moment.locale('ru');
 
 const HeaderCell = styled.div`
   display:flex;
@@ -13,14 +10,13 @@ const HeaderCell = styled.div`
   border-bottom: 1px solid rgba(34,36,38,.1);
   background: #f9fafb;
 `
-const DateCell = React.forwardRef(({ offset }, ref) => {
-    const processedMoment = moment().add(offset, 'days');
+const DateCell = React.forwardRef(({ date }, ref) => {
     /*const dayOfWeek = processedMoment.format('dddd');
     <Header>{dayOfWeek.charAt(0).toLocaleUpperCase() + dayOfWeek.substr(1)}</Header>
     */
     return (
         <HeaderCell ref={ref}>
-            <Header as="h2">{processedMoment.format('DD.MM')}</Header>
+            <Header as="h2">{date.format('DD.MM')}</Header>
         </HeaderCell>
     )
 });
--- src/components/Table.js
+++ src/components/Table.js
@@ -6,9 +6,9 @@ import React, { useRef, useEffect } from 'react';
 
 
 
-const Timetable = styled.div`
+const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(${props => props.days}, minmax(120px,1fr));
+  grid-template-columns: repeat(${props => props.dayCount}, minmax(120px,1fr));
   grid-template-rows: 50px;
   grid-auto-rows: 200px;
   overflow: auto;
@@ -18,12 +18,12 @@ const DayCell = styled.div`
   border-bottom: 1px solid rgba(34,36,38,.1);
   border-right: 1px solid rgba(34,36,38,.1);
   grid-row:2;
-  grid-column:${props => props.col} / ${props => props.col};
+  grid-column:${props => props.col} / ${props => props.col + 1};
 `;
 
 const EventOnGrid = styled(Event)`
   grid-row: 2;
-  grid-column: ${props => props.col}/span ${props => props.length};
+  grid-column: ${props => props.col}/span ${props => props.len};
   align-self:end;
   z-index: 2;
 `;
@@ -32,30 +32,25 @@ const Table = ({ events }) => {
     const currentDayRef = useRef(null);
     useEffect(() => { currentDayRef.current.parentNode.scrollLeft = currentDayRef.current.offsetLeft - currentDayRef.current.parentNode.offsetLeft }, [])
     const renderedDays = 30;
-    const from = renderedDays % 2 === 0 ? Math.round((renderedDays - 2) / 2) : Math.round((renderedDays - 1) / 2); //TODO: Replace with floor
-    const offsets = []; //TODO: shit starts
-    const days = [];
-    const eventsToRender = []; //TODO:convert to an array of moments
-    for (let i = -from, j = 1; i < renderedDays - from; i++, j++) {
-        offsets.push(i);
-        days.push(<DayCell col={j}/>);
-    }
-    const firstRenderedDate = moment().subtract(from, 'days');
-    for (let i = 0; i < events.length; i++) {
-        const col = moment.utc(events[i].startDate).local().diff(firstRenderedDate, 'days') + 1;
-        const len = moment.utc(events[i].endDate).diff(moment.utc(events[i].startDate), 'days') + 1;
-        eventsToRender.push(<EventOnGrid col={col} length={len} color="#4786ff" task="First task" />);
-    } //TODO: shit ends
+    const from = Math.floor((renderedDays - 1) / 2) 
+    const now = moment();
+    const moments = Array.from({ length: renderedDays }, (v, i) => now.clone().add(-from + i, 'days'));
     return (
-        <Timetable days={renderedDays}>
-            {offsets.map(it => {
-                return it === 0 ?
-                    <DateCell id={it} ref={currentDayRef} offset={it} /> :
-                    <DateCell offset={it} />
+        <Grid dayCount={renderedDays}>
+            {moments.map(it => {
+                return it.isSame(now) ?
+                    <DateCell ref={currentDayRef} date={it} /> :
+                    <DateCell date={it} />
             })}
-            {days}
-            {eventsToRender}
-        </Timetable>
+            {moments.map(it => {
+                return <DayCell col={it.diff(now, 'days') + from + 1} />
+            })}
+            {events.map(it => {
+                const col = moment.utc(it.startDate).local().diff(now, 'days') + from;
+                const len = moment.utc(it.endDate).diff(moment.utc(it.startDate), 'days') + 1;
+                return <EventOnGrid col={col} len={len} color="#4786ff" task="test task" />
+            })}
+        </Grid>
     );
 }
 export default Table;
